8df07bf61b8d1b8b4b884d20a484f42388605eba
Faruk Üstünel <farukustunel94@gmail.com>
2019-06-01 11:10:04 +0300

    second commit

--- conf.py
+++ conf.py
@@ -74,6 +74,7 @@ pygments_style = 'manni'
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_logo = "/_static/logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
